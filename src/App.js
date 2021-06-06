2c616b7e32b275c16cf269b4b3e9ffac8eec4ef0
unknown <you@example.com>
2021-06-06 15:42:00 +0600

    home full page is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import FullHomePage from "./Components/HomePage/FullHomePage/FullHomePage";
-
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 function App() {
   return (
-    <div>
-      <FullHomePage></FullHomePage>
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/home'>
+          <FullHomePage></FullHomePage>
+        </Route>
+        <Route path='/'>
+          <FullHomePage></FullHomePage>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- src/Components/HomePage/FullHomePage/FullHomePage.js
+++ src/Components/HomePage/FullHomePage/FullHomePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
 import Appointment from '../Appointment/Appointment';
 import Blog from '../Blog/Blog';
 import Contact from '../Contact/Contact';
@@ -19,6 +20,7 @@ const FullHomePage = () => {
              <Blog></Blog>
              <Doctors></Doctors>
              <Contact></Contact>
+             <Footer></Footer>
         </div>
     );
 };
